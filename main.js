b619c5d1c5c59ed7ffd7ed60bfe3a23b2d7d1767
Mohamed Hassan <mohamedhassanmohamed1@gmail.com>
2022-04-07 14:49:05 +0200

    changes

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 function Person(name, dob) {
     this.name = name;
-    this.age = function() {
+    this.age = function () {
         return new Date(Date.now() - new Date(dob).getTime()).getUTCFullYear() - 1970
     }
 }
 
+Person.prototype.getFullName = function() {
+    return `${this.name} Hassan`;
+}
+
 const test = new Person('Mohamed', '3-15-1980');
 
-console.log(test.age())
+console.log(test.getFullName())
