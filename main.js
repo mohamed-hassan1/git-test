a7a161f8494205778823a8d8f571beb24369aef3
Mohamed Hassan <mohamedhassanmohamed1@gmail.com>
2022-04-07 16:34:52 +0200

    changes

--- main.js
+++ main.js
@@ -10,8 +10,27 @@ class Person {
     getFullName() {
         return `my name is ${this.firstName} ${this.lastName}`;
     }
+    static addNumbers(x,y) {
+        return x+y
+    }
+    static minusNumbers(x,y) {
+        return x-y
+    }
+    
+}
+
+class Customer extends Person {
+    constructor(firstName, lastName, age, services) {
+        super(firstName, lastName, age);
+        this.services = services;
+    }
+
+    getServices() {
+        return `my services is ${this.services}`;
+    }
 }
 
 const test = new Person('Mohamed', 'Hassan', '2-25-1997');
+const test2 = new Customer('Ahmed', 'Shokery', '2-25-1997', 'Doctor');
 
-console.log(test.getFullName())
+console.log(Customer.addNumbers(1,2), Customer.minusNumbers(6,3))
